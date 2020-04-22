23cc531a16ebc670d520281f4c056530f49c2c96
Charles Butler <cdbutle@outlook.com>
2020-04-22 02:30:17 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,8 +30,8 @@ end
   def can_be_created_in_a_block
     Movie.create do |m|
       binding.pry
-    m.title = title
-    m.release_date = release_date
+    m.title = "Home Alone"
+    m.release_date = 1990
     end
   end
 
